9a87611101ce8b18c30788db469082256a24efa4
AkshayPD <akshay@orilla.ai>
2025-08-07 16:44:24 +0530

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export', // Enables static export (for GitHub Pages)
+  output: 'export', // Enables static HTML export
+  distDir: 'docs',  // Output directory for build export
   basePath: isProd ? '/ModularLive' : '',
   assetPrefix: isProd ? '/ModularLive/' : '',
 };
